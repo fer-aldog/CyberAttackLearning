718304cd014312ac963437dc3366f38d8e9c22cd
Justine Joy Conde <146765272+fer-aldog@users.noreply.github.com>
2024-02-28 13:40:15 -0500

    First commit.

---
+++ app/src/main/java/org/pattersonclippers/cybersecuremeapp/ScamsActivity.java
@@ -0,0 +1,14 @@
+package org.pattersonclippers.cybersecuremeapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ScamsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scams);
+    }
+}
