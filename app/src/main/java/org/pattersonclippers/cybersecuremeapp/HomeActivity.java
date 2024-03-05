f922941e6e85b2ef9fa15e72c4e92e26686a48f2
Justine Joy Conde <146765272+fer-aldog@users.noreply.github.com>
2024-03-05 14:29:09 -0500

    Button coding
    Starting to get buttons to work by coding it in Java. Unfinished, still in HomeActivity.java.

--- app/src/main/java/org/pattersonclippers/cybersecuremeapp/HomeActivity.java
+++ app/src/main/java/org/pattersonclippers/cybersecuremeapp/HomeActivity.java
@@ -2,13 +2,31 @@ package org.pattersonclippers.cybersecuremeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+
+    Button homeBTN, malwareBTN, scamBTN, phishingBTN, quizBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        malwareBTN = (Button) findViewById(R.id.malwareBTN);
+        scamBTN = (Button) findViewById(R.id.scamBTN);
+        phishingBTN = (Button) findViewById(R.id.phishingBTN);
+        quizBTN = (Button) findViewById(R.id.quizBTN);
+
+        malwareBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(HomeActivity.this, MalwareActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
--- app/src/main/java/org/pattersonclippers/cybersecuremeapp/MainActivity.java
+++ app/src/main/java/org/pattersonclippers/cybersecuremeapp/MainActivity.java
@@ -2,13 +2,28 @@ package org.pattersonclippers.cybersecuremeapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button doneBTN;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        doneBTN = (Button) findViewById(R.id.doneBTN);
+
+        doneBTN.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(myIntent);
+            }
+        });
     }
 }
